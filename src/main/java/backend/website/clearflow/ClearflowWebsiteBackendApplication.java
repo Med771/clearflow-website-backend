a03b468a5a21d641811eaa47aff1497f09ec7d0d
Med <mm7737333@gmail.com>
2026-02-24 20:46:16 +0300

    Init commit

---
+++ src/main/java/backend/website/clearflow/ClearflowWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.website.clearflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearflowWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearflowWebsiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/website/clearflow/ClearflowWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.website.clearflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearflowWebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
